80eaf42db5ac38f39fecfe124db4e4c417901592
Pieter Smith <pieter@bsentient.ai>
2021-05-22 10:01:49 +0200

    use to heroku postgres-db if available

--- poetische_metode_internationale_taal/settings.py
+++ poetische_metode_internationale_taal/settings.py
@@ -91,15 +91,20 @@ WSGI_APPLICATION = 'poetische_metode_internationale_taal.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    # https://devcenter.heroku.com/articles/heroku-postgresql#connecting-with-django
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.config()
     }
-}
-
+else:
+    # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
