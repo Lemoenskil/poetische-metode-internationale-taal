f982ccf9d3363079fd9a29fd1d3a2dafc7e350c5
Pieter Smith <pieter@bsentient.ai>
2021-06-13 09:07:26 +0200

    add tools to replace local db with heroku db

---
+++ heroku/sync-and-replace-with-heroku-db.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+set -x
+
+HEROKU_DIR=$(dirname $0)
+source ${HEROKU_DIR}/env
+TOP_DIR=${HEROKU_DIR}/..
+CURRENT_TIME=$(date "+%Y.%m.%d-%H.%M.%S")
+
+cd ${TOP_DIR}
+${HEROKU_DIR}/dump-heroku-db.sh
+mv db.sqlite3 backups/db.${CURRENT_TIME}.sqlite3
+python3 manage.py migrate
+${HEROKU_DIR}/import-heroku-db.sh
