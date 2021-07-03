c8ee8594b51e99bc509a8bc82ea6aa6536b2166b
Yvette Smith <yvette@boesman.nl>
2021-07-03 12:32:35 +0200

    add rudimentary genre, author and country grouping

--- poems/views.py
+++ poems/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Poem
+from .models import Poem, Genre, Author, Country
 
 # Create your views here.
 def poems(request):
     """A view that displays the poems page"""
     poems = Poem.objects.all()
-    return render(request, "poems.html", { 'poems': poems })
+    genres = Genre.objects.all()
+    authors = Author.objects.all()
+    countries = Country.objects.all()
+    return render(request, "poems.html", {
+        'poems': poems,
+        'genres': genres,
+        'authors': authors,
+        'countries': countries
+    })
 
 def single_poem(request, id):
     """A view that displays a single poem"""
