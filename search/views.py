d5300535c2a68b1824ae353c57ae5d30a150b6fc
Yvette Smith <yvette@boesman.nl>
2021-07-07 22:44:36 +0200

    add stanzas to search results

--- search/views.py
+++ search/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
 from django.db.models import CharField, TextField
 from django.db.models import Q
-from poems.models import Poem, Author, Genre
+from poems.models import Poem, Author, Genre, Line
 from blogs.models import Blog
 
 def any_model_string_field_containing(model, string, prefix=None):
     prefix = '' if prefix is None else f'{prefix}__'
     fields = [ x for x in model._meta.fields if isinstance(x, CharField) or isinstance(x, TextField) ]
     queries = [ Q( **{ f'{prefix}{x.name}__contains': string } ) for x in fields ]
-    print(queries)
     q_any_of = Q()
     for q in queries:
         q_any_of = q_any_of | q
@@ -26,8 +25,9 @@ def search(request):
         poems = Poem.objects.filter(
             any_model_string_field_containing(Poem, searched) |
             any_model_string_field_containing(Author, searched, 'author') |
-            any_model_string_field_containing(Genre, searched, 'genre')
-        )
+            any_model_string_field_containing(Genre, searched, 'genre') |
+            any_model_string_field_containing(Line, searched, 'stanzas__lines')
+        ).distinct()
 
         return render(request, "search_results.html", {
             'searched': searched,
