d81a13c99b7623ee52ee428461536956e96e2210
Yvette Smith <yvette@boesman.nl>
2021-08-27 19:12:32 +0200

    add catalog to poem detail

--- poems/views.py
+++ poems/views.py
@@ -7,7 +7,6 @@ def poems(request):
     """A view that displays the poems page"""
     poems = Poem.objects.all()
     recent_poems = Poem.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-    print(recent_poems)
     genres = Genre.objects.all()
     authors = Author.objects.all()
     countries = Country.objects.all()
@@ -22,4 +21,16 @@ def poems(request):
 def single_poem(request, id):
     """A view that displays a single poem"""
     poem = get_object_or_404(Poem, pk=id)
-    return render(request, "single-poem.html", { 'poem': poem })
+    poems = Poem.objects.all()
+    recent_poems = Poem.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    genres = Genre.objects.all()
+    authors = Author.objects.all()
+    countries = Country.objects.all()
+    return render(request, "single-poem.html", {
+        'poem': poem,
+        'recent_poems': recent_poems,
+        'poems': poems,
+        'genres': genres,
+        'authors': authors,
+        'countries': countries
+     })
