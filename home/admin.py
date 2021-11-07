9a4c77e6330ddc7ed2cf25b58398521dca799886
Yvette Smith <yvette@boesman.nl>
2021-11-07 12:06:53 +0100

    add editable site introduction

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from translated_fields import TranslatedFieldAdmin
+from nested_admin import NestedModelAdmin
+from .models import SiteSettings
 
 # Register your models here.
+@admin.register(SiteSettings)
+class SiteSettingsAdmin(TranslatedFieldAdmin, NestedModelAdmin):
+    pass
--- home/models.py
+++ home/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from translated_fields import TranslatedField
+from django.utils.translation import gettext_lazy as _
+
+class SingletonModel(models.Model):
+
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        self.pk = 1
+        super(SingletonModel, self).save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        pass
+
+    @classmethod
+    def load(cls):
+        obj, created = cls.objects.get_or_create(pk=1)
+        return obj
 
 # Create your models here.
+class SiteSettings(SingletonModel):
+    """
+    The site settings
+    """
+    introduction = TranslatedField(
+        models.TextField(_("introduction"), default='', blank=True)
+    )
